9c0be0e90be485bb3816efab9ac9e306feffa000
[naresh223@gmail.com] <naresh223@gmail.com>
2023-02-10 20:37:26 +0530

    Added BrowserTest.java file

--- Selenium_Test_Automation/src/test/java/com/application/scripts/BrowserTest.java
+++ Selenium_Test_Automation/src/test/java/com/application/scripts/BrowserTest.java
@@ -2,4 +2,20 @@ package com.application.scripts;
 
 public class BrowserTest {
 
+	
+	public void launchBrowser() {
+
+	}
+
+	/**
+	 * This method is used to demonstrate all the actions that we do on a browser using Selenium WebDriver methods
+	 **/
+	public void browserMethods() {
+
+	}
+
+	public void closeBrowser() {
+
+	}
+
 }
