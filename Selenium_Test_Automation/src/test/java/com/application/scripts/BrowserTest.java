ddceeee5a2a65a027086502a460a907db4d67e4f
[naresh223@gmail.com] <naresh223@gmail.com>
2023-02-10 21:00:57 +0530

    Added browser related selenium methods

--- Selenium_Test_Automation/src/test/java/com/application/scripts/BrowserTest.java
+++ Selenium_Test_Automation/src/test/java/com/application/scripts/BrowserTest.java
@@ -1,21 +1,46 @@
 package com.application.scripts;
 
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
+
 public class BrowserTest {
 
+	WebDriver driver;
 	
+	@BeforeClass
 	public void launchBrowser() {
-
+		System.setProperty("webdriver.chrome.driver", "./drivers/chromedriver.exe");
+		driver = new ChromeDriver();
+		driver.get("https://www.mycontactform.com/");
+		driver.manage().window().maximize();
 	}
 
 	/**
 	 * This method is used to demonstrate all the actions that we do on a browser using Selenium WebDriver methods
+	 * @throws InterruptedException 
 	 **/
-	public void browserMethods() {
-
+	@Test
+	public void browserMethods() throws InterruptedException {
+		System.out.println(driver.getTitle());
+		System.out.println(driver.getCurrentUrl());
+		Thread.sleep(1000);
+		driver.findElement(By.linkText("Sample Forms")).click();
+		System.out.println(driver.getCurrentUrl());
+		driver.navigate().back();
+		Thread.sleep(1000);
+		driver.navigate().forward();
+		Thread.sleep(1000);
+		driver.navigate().refresh();
+		//System.out.println(driver.getPageSource());
 	}
 
+	@AfterClass
 	public void closeBrowser() {
-
+		driver.quit();
 	}
 
 }
